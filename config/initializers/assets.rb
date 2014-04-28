1ba3a55198f7ae079af5bf024f4969ae76587598
Benaouda <benaouda.faroudj@gmail.com>
2014-04-28 13:59:26 +0200

    Commit du siecle

---
+++ config/initializers/assets.rb
@@ -0,0 +1,5 @@
+Rails.application.config.assets.precompile += %w( supersized.css )
+Rails.application.config.assets.precompile += %w( supersized.shutter.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
+Rails.application.config.assets.precompile += %w( supersized.3.2.7.min.js )
+Rails.application.config.assets.precompile += %w( supersized.shutter.min.js )
